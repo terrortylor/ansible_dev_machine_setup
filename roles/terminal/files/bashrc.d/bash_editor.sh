cbdd527656a1b9636cf41213707c0296c4169359
Alex Tylor <alex.tylor@e2x.co.uk>
2019-08-21 11:58:10 +0100

    Add kubectl and google cloud sdk for ubuntu

--- roles/terminal/files/bashrc.d/bash_editor.sh
+++ roles/terminal/files/bashrc.d/bash_editor.sh
@@ -3,4 +3,5 @@
 export EDITOR=nvim
 # Set bash to use vim editing, default starts in insert mode
 # user <esc> or <ctrl+[> to enter 'emulated' normal mode
-set -o vi
+# Note: if below is set then to open up editor it's <esc> v not ctrl+x ctrl+e
+#set -o vi
