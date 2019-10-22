3b2117ded25f62d0bae80b4f825f61792acb2c2e
Alex Tylor <alex.tylor@e2x.co.uk>
2019-10-22 20:21:47 +0100

    Add function for searching aliases and functions, add aliases for docker-compose

--- roles/terminal/files/bashrc.d/aliases.sh
+++ roles/terminal/files/bashrc.d/aliases.sh
@@ -1,10 +1,41 @@
+# Map vi[m] to nvim
 alias vim=nvim
+alias vi=nvim
 
-case `uname` in
-  Darwin)
-    # commands for OS X go here
-  ;;
-  Linux)
-    # commands for Linux go here
-  ;;
-esac
+# docker-compose docker
+alias dcu="docker-compose up -d"
+alias dcul="docker-compose up && docker-compose logs -f"
+alias dcd="docker-compose down"
+
+# Function to search ~/.bashrc.d for any aliases based on a key
+# word; it then prints out the alias name and value on seperate
+# lines to aid in readability.
+function find-alias {
+  if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters, expected 1 got $#"
+    return 0
+  fi
+  echo "Searching for term: $1 in .bashrc.d"
+  # Search aliases
+  while read -r line; do
+    if [[ ${line} == *"alias"* ]]; then
+      found_entry=$(echo $line | cut -d ' ' -f2-)
+      alias_name=$(echo $found_entry | cut -d '=' -f1)
+      alias_value=$(echo $found_entry | cut -d '=' -f2-)
+      RED='\033[0;36m'
+      NC='\033[0m' # No Color
+      printf "alias ${RED}${alias_name}${NC}\n\t${alias_value}\n"
+    fi
+  done < <( grep -ih "^alias.*${1}.*" ~/.bashrc.d/* )
+
+  # Search functions
+  while read -r line; do
+    if [[ ${line} == *"alias"* ]]; then
+      file_name=$(echo $line | cut -d ':' -f1,2)
+      function_name=$(echo $line | cut -d ' ' -f2)
+      RED='\033[0;36m'
+      NC='\033[0m' # No Color
+      printf "function ${RED}${function_name}${NC}\n\tFile: ${file_name}\n"
+    fi
+  done < <( grep -in "^function.*${1}.*" ~/.bashrc.d/* )
+}
