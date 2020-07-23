d4f44fa8ba1845f7e4e9dd50dc43d8a2df533988
Alex Tyor <alex.tylor@e2x.co.uk>
2020-07-23 16:40:10 +0100

    Change RVM to be single-user install

--- roles/ruby/test/integration/fedora/default_spec.rb
+++ roles/ruby/test/integration/fedora/default_spec.rb
@@ -1,14 +1,14 @@
-describe file('/usr/local/rvm/bin/rvm') do
+describe file('/home/vagrant/.rvm/bin/rvm') do
   it { should exist }
 end
 
 %w[ruby-2.5.1 ruby-2.7.0].each do |ruby|
-  describe file("/usr/local/rvm/rubies/#{ruby}") do
+  describe file("/home/vagrant/.rvm/rubies/#{ruby}") do
   it { should exist }
   end
 end
 
-describe file('/usr/local/rvm/gems/default') do
+describe file('/home/vagrant/.rvm/gems/default') do
   it { should be_symlink }
-  its('link_path') { should eq '/usr/local/rvm/gems/ruby-2.7.0' }
+  its('link_path') { should eq '/home/vagrant/.rvm/gems/ruby-2.7.0' }
 end
