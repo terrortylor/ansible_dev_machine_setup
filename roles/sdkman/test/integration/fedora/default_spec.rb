cba1fe851298b6e53b905f3191227c9c3bc05f25
alex.tylor <alex.tylor@e2x.co.uk>
2020-08-10 20:40:59 +0100

    Add sdkman support, currently only java managed

---
+++ roles/sdkman/test/integration/fedora/default_spec.rb
@@ -0,0 +1,15 @@
+describe file('/home/vagrant/.sdkman') do
+  it { should exist }
+  it { should be_directory }
+end
+
+describe file('/home/vagrant/.sdkman/etc/config') do
+  it { should exist }
+  its('content') { should match /^sdkman_auto_answer=true$/ }
+end
+
+%w[11.0.8-zulu].each do |java|
+  describe file("/home/vagrant/.sdkman/candidates/java/#{java}") do
+  it { should exist }
+  end
+end
