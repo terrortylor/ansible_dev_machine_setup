488af96eba8dae8013399c2d41f11db71ab9b174
alex.tylor <alex.tylor@e2x.co.uk>
2021-01-11 16:01:01 +0000

    NPM global path fixup

--- roles/dotfiles/files/bashrc.d/npm.sh
+++ roles/dotfiles/files/bashrc.d/npm.sh
@@ -1,9 +1,16 @@
-case `uname` in
-  Darwin)
-    # commands for OS X go here
-  ;;
-  Linux)
-    # commands for Linux go here
-    export PATH=~/.npm-global/bin:$PATH
-  ;;
+if [ command -v npm &> /dev/null ]; then
+  case `uname` in
+    Darwin)
+      # commands for OS X go here
+      ;;
+    Linux)
+      export NPM_CONFIG_PREFIX="${HOME}/.npm-global"
+      if [ ! -d $NPM_CONFIG_PREFIX ]; then
+        mkdir $NPM_CONFIG_PREFIX
+      fi
+
+      # commands for Linux go here
+      export PATH=$NPM_CONFIG_PREFIX:$PATH
+    ;;
 esac
+fi
